8822d78e305731fb92bb1819520ccd22a0b48dc3
whatcq <gdaymate.liu@gmail.com>
2018-03-31 17:36:18 +0800

    fix getenv under SAPI

--- testbase.php
+++ testbase.php
@@ -1,12 +1,9 @@
 <?php
 function getClientIp()
 {
-    if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "127.0.0.1"))
-        $ip = getenv("HTTP_CLIENT_IP");
-    else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "127.0.0.1"))
-        $ip = getenv("HTTP_X_FORWARDED_FOR");
-    else if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "127.0.0.1"))
-        $ip = getenv("REMOTE_ADDR");
+    if ($ip = getenv("HTTP_CLIENT_IP") && strcasecmp($ip, "127.0.0.1"));
+    else if ($ip = getenv("HTTP_X_FORWARDED_FOR") && strcasecmp($ip, "127.0.0.1"));
+    else if ($ip = getenv("REMOTE_ADDR", true) && strcasecmp($ip, "127.0.0.1"));
     else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "127.0.0.1"))
         $ip = $_SERVER['REMOTE_ADDR'];
     else
