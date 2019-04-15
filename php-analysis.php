c37a4bd7cfb939812b4704a65e69d6ea97a28ced
whatcq <gdaymate.liu@gmail.com>
2019-04-15 14:00:31 +0800

    fix bug

--- php-analysis.php
+++ php-analysis.php
@@ -17,13 +17,13 @@ register_shutdown_function(function () {
     $stats = [
         'runtime'   => round(microtime(true) - $GLOBALS['cqiu__startTime'], 3).' s',
         'memory'    => sprintf('%.3f MB', memory_get_peak_usage() / 1048576),
-        'files'     => strpos($_GET['t'], 'x')!==false ? 'x' : get_included_files(),
-        'constants' => strpos($_GET['t'], 'T')!==false ? 'T' : get_defined_constants(),
-        'functions' => strpos($_GET['t'], 'F')===false ? 'F' : get_defined_functions()['user'],
-        'classes'   => strpos($_GET['t'], 'C')===false ? 'C' : get_declared_classes(),
-        'get'   => strpos($_GET['t'], 'g')!==false ? 'g' : $_GET,
-        'post'   => strpos($_GET['t'], 'p')!==false ? 'p' : $_POST,
-        'server'   => strpos($_GET['t'], 's')!==false ? 's' : $_SERVER,
+        'files'     => strpos($_GET['_t'], 'x')!==false ? 'x' : get_included_files(),
+        'constants' => strpos($_GET['_t'], 'T')===false ? 'T' : get_defined_constants(),
+        'functions' => strpos($_GET['_t'], 'F')===false ? 'F' : get_defined_functions()['user'],
+        'classes'   => strpos($_GET['_t'], 'C')===false ? 'C' : get_declared_classes(),
+        'get'   => strpos($_GET['_t'], 'g')===false ? 'g' : $_GET,
+        'post'   => strpos($_GET['_t'], 'p')===false ? 'p' : $_POST,
+        'server'   => strpos($_GET['_t'], 's')===false ? 's' : $_SERVER,
     ];
     echo '<pre>';
     print_r($stats);
