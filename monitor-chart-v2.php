5dd434fb49e867604d972abc863b57da952afd86
whatcq <gdaymate.liu@gmail.com>
2019-09-25 15:30:12 +0800

    monitor-chart by eventSource

--- monitor-chart-v2.php
+++ monitor-chart-v2.php
@@ -2,11 +2,14 @@
 /**
  * web监控php-fpm status数据
  * @author Cqiu
- * @date 2018-1-5
+ * @date 2019-9-25
  */
 
 if (isset($_REQUEST['data'])) {
-    $url = 'http://xxx.com/status';
+    date_default_timezone_set("Asia/Chongqing");
+    header("Content-Type: text/event-stream\n\n");
+
+    $url = 'http://xxxx.com/status';
 
     $r = [
         'listen_queue' => 0,
@@ -18,36 +21,37 @@ if (isset($_REQUEST['data'])) {
         //'max_active_processes' => 0,
         //'max_children_reached' => 0
     ];
-    $result = json_decode(file_get_contents($url . '?json'), 1);
-    foreach ($result as $k => $v) {
-        $k = strtr($k, ' ', '_');
-        isset($r[$k]) && $r[$k] = $v;
-    }
 
-    echo json_encode($r);
-    exit;
+    $ctx = stream_context_create([
+        'http' => [
+            'timeout' => 2,
+        ],
+    ]);
+
+    while (1) {
+        $result = json_decode(file_get_contents($url . '?json', 0, $ctx), 1);
+        foreach ($result as $k => $v) {
+            $k = strtr($k, ' ', '_');
+            isset($r[$k]) && $r[$k] = $v;
+        }
+        echo 'data: ', json_encode($r), "\n\n";
+
+        ob_flush();
+        flush();
+        sleep(1);
+    }
 }
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
     <meta charset="utf-8">
-    <title>php-fpm status监控</title>
     <link rel="shortcut icon" href="https://tool.lu/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        /* css 代码  */
-    </style>
-    <!-- <script src="https://img.hcharts.cn/jquery/jquery-1.8.3.min.js"></script> -->
-    <script src="https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/highstock/6.0.3/highstock.js"></script>
-    <!-- <script src="https://img.hcharts.cn/highstock/highstock.js"></script> -->
-    <!-- <script src="https://img.hcharts.cn/highcharts/modules/exporting.js"></script> -->
-    <!-- <script src="https://img.hcharts.cn/highcharts-plugins/highcharts-zh_CN.js"></script> -->
 </head>
 <body>
 <div id="container" style="min-width:400px;height:400px"></div>
-<input type="button" id="on_off" value="start"/>
 <script>
     var dataQueue = [];
     Highcharts.setOptions({
@@ -57,10 +61,8 @@ if (isset($_REQUEST['data'])) {
     });
 
     // 填充初始值，才能有宽度
-    var fillData = []
-        , time = (new Date()).getTime()
-        , i;
-    for (i = -60; i <= 0; i += 1) {
+    var fillData = [], time = (new Date()).getTime();
+    for (var i = -60; i <= 0; i += 1) {
         fillData.push([time + i * 1000, 0]);
     }
 
@@ -151,31 +153,10 @@ if (isset($_REQUEST['data'])) {
         }]
     });
 
-    var timer = null;
-
-    $.ajaxSetup({
-        timeout: 2000, // 2s
-        complete: function (XMLHttpRequest, textStatus) {
-            if (textStatus == 'timeout') {
-                console.log('timeout')
-            }
-        }
-    });
-
-    $('#on_off').click(function () {
-        var o = $(this);
-        if (o.val() == 'stop') {
-            o.val('start');
-            clearInterval(timer);
-        } else {
-            o.val('stop');
-            timer = setInterval(function () {
-                $.getJSON('?data=' + Math.random(), function (result) {
-                    dataQueue.push(result);
-                });
-            }, 1000);
-        }
-    });
+    var evtSource = new EventSource('?data');
+    evtSource.onmessage = function (e) {
+        dataQueue.push(JSON.parse(e.data));
+    }
 </script>
 </body>
 </html>
