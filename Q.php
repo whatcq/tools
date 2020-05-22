e5a65e10b21a36280348fa696984e66ce9ce317a
LSF <gdaymate@126.com>
2020-05-22 17:49:48 +0800

    Q add sql

--- Q.php
+++ Q.php
@@ -16,6 +16,7 @@ $sqls = [
 	'u' => ':user data limit 30',
 	'u#' => ':count user data',
 	'u#2' => ':query user data',
+	'u#*' => ':table structure',
 ];
 foreach ($sqls as $_q => $sql) {
 	echo " <a href=\"?q=", urlencode($_q), "\" title=\"$sql\">$_q</a>";
@@ -42,7 +43,7 @@ function parse($q) {
 
 	if ($q[0] === '-') {
 		return [
-			'SELECT TABLE_SCHEMA,TABLE_NAME, COLUMN_NAME FROM information_schema.columns'
+			'SELECT TABLE_SCHEMA,TABLE_NAME,COLUMN_NAME FROM information_schema.columns'
 			. ' WHERE TABLE_SCHEMA=?s AND COLUMN_NAME LIKE ?s',
 			DB_NAME,
 			substr($q, 1),
@@ -55,6 +56,13 @@ function parse($q) {
 
 	$params = [];
 	if ($id) {
+		if ($id === '*') {
+			return [
+				'SELECT COLUMN_NAME,COLUMN_COMMENT,COLUMN_DEFAULT default_,IS_NULLABLE nul,DATA_TYPE,CHARACTER_MAXIMUM_LENGTH m,CHARACTER_OCTET_LENGTH n,COLUMN_KEY,EXTRA FROM information_schema.`COLUMNS` WHERE TABLE_SCHEMA=?s AND TABLE_NAME=?s', //,CHARACTER_SET_NAME c,COLLATION_NAME,COLUMN_TYPE
+				DB_NAME,
+				$table,
+			];
+		}
 		$sql = "SELECT * FROM $table";
 		$sql .= ' WHERE id=?i';
 		$params = [$id];
@@ -106,13 +114,13 @@ if ($q) {
 
 	require 'lib/DB.php';
 
-	// parse && execute
+	// parse
 	$w = parse($q);
-	// var_dump($w);die;
+	var_dump($w);
+	// execute
 	$r = call_user_func_array('DB::q', $w);
 	$d = $r->fetchAll(PDO::FETCH_ASSOC);
 
-	var_dump($w);
 	echo '<hr />';
 	render($d);
 }
