aaa076a5ba8876efba47676b8b1b7290eb8dbee5
LSF <gdaymate.liu@gmail.com>
2020-06-05 18:10:37 +0800

    qredis filter command

--- qredis.php
+++ qredis.php
@@ -13,7 +13,6 @@ if (isset($_GET['redis_commands'])) {
 require 'lib/RedisClient.php';
 
 $q = isset($_REQUEST['q']) ? $_REQUEST['q'] : null;
-$show = isset($_REQUEST['show']) ? $_REQUEST['show'] : null;
 if (isset($_REQUEST['q'])) {
 	$client = new RedisClient('127.0.0.1', 6379);
 	echo '<pre>';
@@ -48,7 +47,7 @@ tr:nth-child(even),li:nth-child(even) {background-color: #fafafa;}
 tr:hover,li:hover{background: #c3e9cb;}
 pre{margin:0;}
 i{font-size:60%;color:gray;}
-#result{min-height: 20px;max-height: 200px;padding: 10px; background: #f1f0f0; border-radius: 5px;}
+#result{min-height: 20px;max-height: 200px;padding: 10px; background: #f1f0f0; border-radius: 5px;overflow: auto;}
 #commands-container{max-height: 300px;overflow: auto;border: 1px solid #eee;}
 #commands-container li{display: none;color:#a3a3a3;}
 .command{color:blue;}
@@ -81,14 +80,31 @@ function renderRedisCommands(){
 			cmd += ' '+_args;
 		}
 		$('#cmd').val(cmd);
-		$.get('?', {q:cmd}, function(result){
-			$('#result').html(result);
-		})
+		query(cmd);
 	})
+}
+function query(cmd){
+	$.get('?', {q:cmd}, function(result){
+		$('#result').html(result);
+	});
+	return false;
 }
 window.onload = function() {
     $('#q').focus();
     renderRedisCommands();
+	$('#form').submit(function(e){
+		// e.preventDefault();
+    	query($('#cmd').val());
+    	return false;
+    });
+    $('#cmd').change(function(){
+    	var v=$(this).val();
+    	console.log(v)
+    	if(v.length>1 && v.indexOf(' ')<0){
+    		$('#commands-container li').hide();
+    		$('#commands-container li[data-name*='+v+']').show();
+    	}
+    });
 };
 function filterGroup(group){
 	$('#commands-container li').hide();
@@ -98,42 +114,42 @@ function filterGroup(group){
 <div id="result"></div>
 <select style="height:25px" onchange="filterGroup($(this).val())">
 	<option value=''>All</option>
-<optgroup label="常用">
-	<option value='generic'>Keys 键</option>
-	<option value='string'>Strings 字符串</option>
-	<option value='list'>Lists 列表</option>
-	<option value='hash'>Hashes 哈希</option>
-	<option value='set'>Sets 集合</option>
-	<option value='sorted_set'>Sorted Sets 有序集合</option>
+<optgroup label="...">
+	<option value='generic'>Keys - 键</option>
+	<option value='string'>Strings - 字符串</option>
+	<option value='list'>Lists - 列表</option>
+	<option value='hash'>Hashes - 哈希</option>
+	<option value='set'>Sets - 集合</option>
+	<option value='sorted_set'>Sorted Sets - 有序集合</option>
 </optgroup>
+<optgroup label="...">
 	<option value='hyperloglog'>HyperLogLog</option>
 	<option value='geo'>Geo</option>
-	<option value='pubsub'>Pub/Sub</option>
-	<option value='cluster'>Cluster</option>
-	<option value='connection'>Connection</option>
-	<option value='scripting'>Scripting</option>
-	<option value='server'>Server</option>
-	<option value='stream'>Streams</option>
-	<option value='transactions'>Transactions</option>
+	<option value='pubsub'>Pub/Sub - 发布/订阅</option>
+	<option value='cluster'>Cluster - 集群</option>
+	<option value='connection'>Connection - 连接</option>
+	<option value='scripting'>Scripting - 脚本</option>
+	<option value='server'>Server - 服务器</option>
+	<option value='stream'>Streams - 流</option>
+	<option value='transactions'>Transactions - 事务</option>
+</optgroup>
 </select>
 
-<form style="display: inline-block;margin-bottom: 0;">
+<form style="display: inline-block;margin-bottom: 0;" id="form">
 	<div style="position:relative;">
 		<span style="margin-left:200px;width:18px;overflow:hidden;">
 			<select style="width:218px;margin-left:-200px;height: 25px;"
-			 onchange="$('q').value=this.value;$('q').focus();$('show').value=''">
+			 onchange="$('#cmd').val(this.value);$('#cmd').focus();">
+			 <option value=""> </option>
+<?php
+foreach ($shows as $_show) {
+	echo "<option value=\"$_show\"> $_show </option>\n";
+}
+?>
 			</select>
 		</span>
 		<input type="text" name="q" id="cmd" value="<?php echo $q; ?>"
 			style="width:200px;position:absolute;left:2px;top:2px;height: 21px;border:0;" />
-		<select name="show" id="show" style="height: 25px;" onchange="this.form.submit()">
-			<option value="">-- show --</option>
-<?php
-foreach ($shows as $_show) {
-	echo "<option value=\"$_show\"", ($show === $_show ? ' selected' : ''), "> $_show </option>\n";
-}
-?>
-		</select>
 		<input type="submit" value="Go" />
 	</div>
 </form>
