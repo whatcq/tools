5d401a02fbedd8761f772942d6b3b182b7221404
LSF <gdaymate.liu@gmail.com>
2020-07-21 11:42:32 +0800

    add markdown -> mempad tool

---
+++ md2mempad.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * markdown转mempad
+ * 一般markdown不会太大吧，所以没做逐行读取，撇脱
+ * cqiu@2020-7-21
+ */
+define('SP', chr(0));
+
+$file = 'architect.md';
+
+$data = file_get_contents($file);
+$data = preg_replace_callback(
+    '/(?:^|\n)(#+) (.*)/',
+    function ($matches) {
+        return SP . chr(strlen($matches[1])) . $matches[2] . SP;
+    },
+    $data);
+
+$info['initialPage'] = 1;
+$info['quickPagePath'] = '';
+$header = 'MeMpAd.' . $info['initialPage'] . SP . $info['quickPagePath'];
+
+file_put_contents("$file.lsf", $header . $data . SP);
