2f70a283eeda8912288861f5ee69fa0870539331
LSF <gdaymate.liu@gmail.com>
2020-07-21 12:00:23 +0800

    remove br

--- md2mempad.php
+++ md2mempad.php
@@ -6,11 +6,11 @@
  */
 define('SP', chr(0));
 
-$file = 'architect.md';
+$file = 'D:\www\book\architect-awesome\architect.md';
 
 $data = file_get_contents($file);
 $data = preg_replace_callback(
-    '/(?:^|\n)(#+) (.*)/',
+    '/(?:^|\n)(#+) (.*)\r?\n/',
     function ($matches) {
         return SP . chr(strlen($matches[1])) . $matches[2] . SP;
     },
@@ -20,4 +20,4 @@ $info['initialPage'] = 1;
 $info['quickPagePath'] = '';
 $header = 'MeMpAd.' . $info['initialPage'] . SP . $info['quickPagePath'];
 
-file_put_contents("$file.lsf", $header . $data . SP);
+echo file_put_contents("$file.lsf", $header . $data . SP), " ok\n";
