1c5d5fd1b5fb2c7eb64adeb329bd72bb1b0f2a90
LSF <gdaymate.liu@gmail.com>
2020-09-11 17:00:04 +0800

    more rename

--- other/rename.php
+++ other/rename.php
@@ -1,14 +1,16 @@
 <?php
 
 // 合并二进制文件
-// copy /b *.ts xiao.mp4
+// php D:\www\cqiu\cqiu-tools\other\rename.php . & copy /b *.ts %cd%.mp4
 
 PHP_SAPI === 'cli' or die('Usage: php other/rename.php D:/www/xyz');
 
 $folder = isset($argv[1]) ? $argv[1] : '.';
 
+echo $folder;
+
 foreach (glob("$folder/*.ts") as $file) {
-	$newFile = preg_replace_callback('|seg-(\d+)-v1-a1\.ts|', function ($matches) {
+	$newFile = preg_replace_callback('|seg-(\d+)(-f\d)?-v1-a1\.ts|', function ($matches) {
 		return sprintf('seg-%03d-v1-a1.ts', $matches[1]);
 	}, $file);
 	if ($file == $newFile) {
