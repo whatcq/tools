5942f5dea71ef0b7baceb1ae4e6a596d058767fd
Cqiu <gdaymate.liu@gmail.com>
2021-03-16 14:20:09 +0800

    update watch.php

--- watch.php
+++ watch.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * ¼à¿ØphpÎÄ¼þ±ä»¯²¢Ö´ÐÐ&Êä³ö½á¹û
+ * ç›‘æŽ§phpæ–‡ä»¶å˜åŒ–å¹¶æ‰§è¡Œ&è¾“å‡ºç»“æžœ
+ * php watch.php "app/Console/Commands/ImportShippingFee.php" "php artisan import_shipping_fee"
  * @author:Cqiu
  * @date: 2017-6-15 10:39
  */
 
 define('IS_WIN', (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') ? true : false);
-define('IS_GBK', (strlen('ÖÐÎÄ') === 4) ? true : false);
-var_dump(IS_WIN, IS_GBK);
-exit;
+//define('IS_GBK', (strlen('ä¸­æ–‡') === 4) ? true : false);
+//var_dump(IS_WIN, IS_GBK);
+//exit;
 
 if ($argc < 2) {
     start:
@@ -21,20 +22,22 @@ if (!is_file($file)) {
     echo 'File not found : ', $file, PHP_EOL;
     goto start;
 }
+$cmd = isset($argv[2]) ? $argv[2] : "php $file";
+
 echo "monitor start: $file -------------\n";
 $sign = 0;
 while (1) {
     $_sign = md5_file($file);
     if ($_sign !== $sign) {
         if (IS_WIN) {
-            `cls`;
+            //`cls`;//no use
             echo date('H:i:s'), "\n--------------\n";
-            echo @iconv('UTF-8', 'GBK', `php $file`);
+            echo `$cmd`;//@iconv('UTF-8', 'GBK', `$cmd`);
         } else {
-            `clear`;
+            //`clear`;
             echo date('H:i:s'), "\n--------------\n";
             $dir = dirname($file);
-            echo `cd $dir && php $file`;
+            echo `cd $dir && $cmd`;
         }
         $sign = $_sign;
     }
