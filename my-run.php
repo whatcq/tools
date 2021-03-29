2e7ff658bdc14e2f5884380a0f92373639656ebc
Cqiu <gdaymate.liu@gmail.com>
2021-03-29 18:21:52 +0800

    php playground with ace editor

---
+++ my-run.php
@@ -0,0 +1,27 @@
+<title>Run PHP</title>
+<style type="text/css">#e{position: absolute;top:0;right:0;bottom:0;left:0;font-size:16px;}</style>
+<div id="e">&lt;?php
+var_dump(
+    1
+);
+</div>
+<div style="position: fixed; right: 0; top: 0;width: 700px; height: 100%;">
+    <form method="post" action="playground.php?act=save_run" target="iframe" style="display:inline;">
+        <input type="text" name="filename" id="filename" value="test" style="float:right">
+        <textarea name="source" id="source" cols="30" rows="10" style="display:none;"></textarea>
+    </form>
+    <iframe src="" name="iframe" frameborder="0" style="width: 100%;height: 100%;"></iframe>
+</div>
+<script src="https://cdn.bootcss.com/ace/1.4.6/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    var e = ace.edit("e");
+    e.setTheme("ace/theme/monokai");
+    e.getSession().setMode("ace/mode/php");
+    document.onkeydown = function (e) {
+        if (e.key==='s' && e.ctrlKey) {
+            document.getElementById('source').value=window.e.getValue();
+            document.forms[0].submit();
+            return false;
+        }
+    };
+</script>
