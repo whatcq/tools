a3317db282cdf06b3367220343fb7f7d08381b6d
cqiu <gdaymate@126.com>
2022-02-18 16:39:53 +0800

    cache js to local static

---
+++ other/down.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+curl https://unpkg.com/jquery@3.6.0/dist/jquery.min.js -o jquery-3.6.0.min.js
+curl https://unpkg.com/vue@3.2.29/dist/vue.global.prod.js -o vue-3.2.29.global.prod.js
+
+curl https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.jquery.min.js -O
