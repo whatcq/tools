3e0b86ed487875f8e679ba16bac23f00225f9549
cqiu <gdaymate@126.com>
2022-03-31 17:59:54 +0800

    my-run edit file with format

--- my-run.php
+++ my-run.php
@@ -1,14 +1,26 @@
 <title>Run PHP</title>
 <style type="text/css">#e{position: absolute;top:0;right:0;bottom:0;left:0;font-size:16px;}</style>
-<div id="e"><?php !empty($_SERVER['QUERY_STRING'])
-    && file_exists($file = "playground/{$_SERVER['QUERY_STRING']}.php")
-    && print(htmlspecialchars(file_get_contents($file)))
+<div id="e"><?php
+    function getCode($file)
+    {
+        $source = file_get_contents($file);
+        if (isset($_REQUEST['format']) || isset($_REQUEST['ff'])) {
+            include 'lib/php-formatter.php';
+            $formatter = new Formatter();//配置：['braceNewline' => [T_FUNCTION, T_CLASS]]
+            $source = $formatter->format($source);
+        }
+        return $source;
+    }
+
+    !empty($_REQUEST['f'])
+    && file_exists($file = "playground/{$_REQUEST['f']}.php")
+    && print(htmlspecialchars(getCode($file)))
     or print("&lt;pre style=\"color:green\">&lt;?php\nvar_dump(\n    1\n);");
     ?>
 </div>
 <div style="position: fixed; right: 0; top: 0;max-width: 50%;width: 700px; height: 100%;">
     <form method="post" action="playground.php?act=save_run" target="iframe" style="display:inline;">
-        <input type="text" name="filename" id="filename" value="<?=$_SERVER['QUERY_STRING']?:'test'?>" style="position: fixed;right: 20px;">
+        <input type="text" name="filename" id="filename" value="<?=$_REQUEST['f']?:'test'?>" style="position: fixed;right: 20px;">
         <textarea name="source" id="source" cols="30" rows="10" style="display:none;"></textarea>
     </form>
     <iframe src="" name="iframe" frameborder="0" style="width: 100%;height: 100%;"></iframe>
