98a320feec77742eadbce0a82d8859b0c65939e7
cqiu <gdaymate@126.com>
2022-07-12 17:03:24 +0800

    optimize: _err_getsource

--- php-analysis.php
+++ php-analysis.php
@@ -27,37 +27,30 @@ function _err($msg)
     {
         if (preg_match('/\<\?(php)?[^[:graph:]]/i', $code)) {
             return highlight_string($code, TRUE);
-        } else {
-            return preg_replace('/(&lt;\?php&nbsp;)+/i', "", highlight_string("<?php " . $code, TRUE));
         }
+        return str_replace('&lt;?php&nbsp;', '', highlight_string("<?php " . $code, TRUE));
     }
 
     function _err_getsource($file, $line)
     {
-        if (!(file_exists($file) && is_file($file))) {
+        if (!is_file($file)) {
             return '';
         }
-        $data = file($file);
-        $count = count($data) - 1;
-        $start = $line - 5;
-        if ($start < 1) {
-            $start = 1;
-        }
-        $end = $line + 5;
-        if ($end > $count) {
-            $end = $count + 1;
-        }
-        $returns = array();
-        for ($i = $start; $i <= $end; $i++) {
-            if ($i == $line) {
-                $returns[] = "<div id='current'>" . $i . ".&nbsp;" . _err_highlight_code($data[$i - 1], TRUE) . "</div>";
-            } else {
-                $returns[] = $i . ".&nbsp;" . _err_highlight_code($data[$i - 1], TRUE);
-            }
+        $fp = fopen($file, 'r');
+        $p = max($line - 5, 1);
+        $returns = [];
+        $i = 1;
+        while ($i++ < $p) fgets($fp, 4096);
+        while ($p < $line + 5) {
+            if (false === $codeLine = fgets($fp, 4096)) break;
+            $returns[] = $p !== $line
+                ? "<i>$p</i> " . _err_highlight_code($codeLine)
+                : "<div class='current'><i>$p</i>" . _err_highlight_code($codeLine) . "</div>";
+            $p++;
         }
         return $returns;
     }
-    ?><!DOCTYPE html><html lang="zh-cn"><head><meta name="robots" content="noindex, nofollow, noarchive" /><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title><?php echo $msg;?></title><style>body{padding:0;margin:0;word-wrap:break-word;word-break:break-all;font-family:Courier,Arial,sans-serif;background:#EBF8FF;color:#5E5E5E;}div,h2,p,span{margin:0; padding:0;}ul{margin:0; padding:0; list-style-type:none;font-size:0;line-height:0;}#body{margin:0 auto;}#main{width:95%;margin:13px auto 0 auto;padding:0 0 35px 0;}#contents{margin:13px auto 0 auto;background:#FFF;padding:8px 0 0 9px;}#contents h2{display:block;background:#CFF0F3;font:bold 20px Arial;padding:12px 0 12px 30px;margin:0 10px 22px 1px;}#contents ul{padding:0 0 0 18px;font-size:0;line-height:0;}#contents ul li{display:block;padding:0;color:#8F8F8F;background-color:inherit;font:normal 14px Arial, Helvetica, sans-serif;margin:0;}#contents ul li span{display:block;color:#408BAA;background-color:inherit;font:bold 14px Arial, Helvetica, sans-serif;padding:0 0 10px 0;margin:0;}#oneborder{width:auto;font:normal 14px Arial, Helvetica, sans-serif;border:#EBF3F5 solid 4px;margin:0 30px 20px 30px;padding:10px 20px;line-height:110%;}#oneborder span{padding:0;margin:0;}#oneborder #current{background:#CFF0F3;}code{font-family:Courier,Arial,sans-serif;}</style></head><body><div id="main"><div id="contents"><h2><?php echo $msg?></h2><?php foreach($traces as $trace){if(is_array($trace)&&!empty($trace["file"])){$souceline = _err_getsource($trace["file"], $trace["line"]);if($souceline){?><ul><li><span><?php echo $trace["file"];?> on line <?php echo $trace["line"];?> </span></li></ul><div id="oneborder"><?php foreach($souceline as $singleline)echo $singleline;?></div><?php }}}?></div></div><div style="clear:both;padding-bottom:50px;" /></body></html><?php
+?><!DOCTYPE html><html lang="zh-cn"><head><meta name="robots" content="noindex, nofollow, noarchive" /><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title><?php echo $msg;?></title><style>body{padding:0;margin:0;word-wrap:break-word;word-break:break-all;font-family:Courier,Arial,sans-serif;background:#EBF8FF;color:#5E5E5E;}div,h2,p,span{margin:0; padding:0;}ul{margin:0; padding:0; list-style-type:none;font-size:0;line-height:0;}#body{margin:0 auto;}#main{width:95%;margin:13px auto 0 auto;padding:0 0 35px 0;}#contents{margin:13px auto 0 auto;background:#FFF;padding:8px 0 8px 9px;}#contents h2{display:block;background:#CFF0F3;font:bold 20px Arial;padding:12px 0 12px 30px;margin:0 10px 22px 1px;}#contents ol{padding:0 0 0 18px;font-size:0;line-height:0;}#contents ol li{padding:0;color:#8F8F8F;background-color:inherit;font:normal 14px Arial, Helvetica, sans-serif;margin:0;}#contents ol li span{display:block;color:#408BAA;background-color:inherit;font:bold 14px Arial, Helvetica, sans-serif;padding:0 0 10px 0;margin:0;}.code-block{width:auto;font:normal 14px Arial, Helvetica, sans-serif;border:#EBF3F5 solid 4px;margin:0 30px 20px 0;padding:10px 20px;line-height:110%;}.code-block span{padding:0;margin:0;}.code-block .current{background:#CFF0F3;}code{font-family:Courier,Arial,sans-serif;}.code-block i{color:#bebebe;margin-right: 10px;}</style></head><body><div id="main"><div id="contents"><h2><?php echo $msg?></h2><ol><?php foreach($traces as $trace){if(is_array($trace)&&!empty($trace["file"])){$sourceLine = _err_getsource($trace["file"], $trace["line"]);if($sourceLine){?><li><span><?php echo $trace["file"];?> on line <?php echo $trace["line"];?> </span></li><div class="code-block"><?php foreach($sourceLine as $singleLine)echo $singleLine;?></div><?php }}}?></ol></div></div><div style="clear:both;padding-bottom:50px;" /></body></html><?php
     exit;
 }
 if(!empty($_COOKIE['_trace']) || !empty($_REQUEST['_trace'])) {
@@ -72,7 +65,7 @@ if(!empty($_COOKIE['_trace']) || !empty($_REQUEST['_trace'])) {
  * - 无参数：返回所有
  * - 一个普通值参数：直接打印
  * - 数组|对象|多参数：print_r
- * @todo 界面上ajax 注释/删除 掉测试代码
+ * @todo 通过界面 注释/删除 掉测试代码
  */
 function _log()
 {
@@ -148,12 +141,7 @@ register_shutdown_function(function () use ($debugOptions) {
     isset($selectPanels['s']) && $traces['server'] = $_SERVER;
     $logs && $traces['vars'] = $logs;
     empty($GLOBALS['_']) or $traces['vars2'] = $GLOBALS['_'];
-    // echo '<pre>';
-    // print_r($traces);
-    // echo '</pre>';
-
-    //@todo 优化一下下面的前端实现代码
-    ?>
+?>
 <style>
 small{font-size: 60%}
 #debugBar{padding:0;position:fixed;bottom:0;right:0;font-size:14px;width:100%;z-index:999999;color:#000;text-align:left;font-family:'微软雅黑',serif}
