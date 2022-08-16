e3f3cc48ad9f021771a97f4f9bb500ab1877854b
cqiu <gdaymate@126.com>
2022-08-16 08:08:26 +0800

    mode assistant

--- tasks/bots/0001-index.php
+++ tasks/bots/0001-index.php
@@ -1,5 +1,12 @@
 <?php
 
+
+if (!empty($_SESSION['mode']) && in_array($text, ['不玩了', '退出', '我不想玩了', '再见', '拜拜', '喜欢土豆。'])) {
+    $_SESSION['mode'] = null;
+    return '好的！bye';
+}
+
 if (strpos($text, '开始成语接龙') !== false) $_SESSION['mode'] = '成语接龙';
+elseif (strpos($text, '在搞啥子') !== false && strlen($text) < 30) $_SESSION['mode'] = '助手';
 
 return; // 一定要，否则默认会return true;
--- tasks/bots/0100-search-engine.php
+++ tasks/bots/0100-search-engine.php
@@ -12,13 +12,6 @@ accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/a
 accept-encoding: gzip
 accept-language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6
 cookie: sm_diu=94463918377ae9dbcd01540306f8101f%7C%7C11eef1ee73e005ee0f%7C1659966719; sm_uuid=66fa6a973a09f00e6f24e12befb29f69%7C%7C%7C1659966787; __itrace_wid=8a245aae-8297-4e15-a368-ae00d1d6cbfc; sm_ruid=f69c2a151e5e575806ed3af33baceaf0%7C%7C%7C1659970745; lsmap2=1a03M14U07S1CE1Nn0Ps0Tx0Wk0Yw0900; isg=BDQ0YRXX6pZgiH5aa_gQO176BfKmDVj39HPGYc6UD7_nOdaD9B3qhzqwuTFEwZBP
-sec-ch-ua: "Chromium";v="104", " Not A;Brand";v="99", "Microsoft Edge";v="104"
-sec-ch-ua-mobile: ?0
-sec-ch-ua-platform: "Windows"
-sec-fetch-dest: document
-sec-fetch-mode: navigate
-sec-fetch-site: none
-sec-fetch-user: ?1
 upgrade-insecure-requests: 1
 user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54
 HEADERS,
@@ -81,13 +74,6 @@ Connection: keep-alive
 Cookie: SUID=626C55773322910A00000000618760E8; SUV=1636262121148199; SMYUV=1636262249981327; ssuid=3715243536; usid=056A5577ED18A00A0000000061AC1073; SGINPUT_UPSCREEN=1650155291059; sg_client_ip=119.85.107.228; IPLOC=CN5000; ABTEST=6|1658019236|v17; browerV=3; osV=1; SNUID=B91B240F7177976655FDDA727271B45A; ld=Vyllllllll2AZCEulllllpacyDylllllNCqouZllll9lllllRklll5@@@@@@@@@@; sst0=13
 Host: www.sogou.com
 Referer: https://www.sogou.com/
-sec-ch-ua: "Chromium";v="104", " Not A;Brand";v="99", "Microsoft Edge";v="104"
-sec-ch-ua-mobile: ?0
-sec-ch-ua-platform: "Windows"
-Sec-Fetch-Dest: document
-Sec-Fetch-Mode: navigate
-Sec-Fetch-Site: same-origin
-Sec-Fetch-User: ?1
 Upgrade-Insecure-Requests: 1
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54
 HEADERS,
@@ -116,19 +102,6 @@ accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/a
 accept-encoding: gzip
 accept-language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6
 cookie: _EDGE_V=1; MUID=3F7FD7CCFB276E3321A3C743FA096FDE; MUIDB=3F7FD7CCFB276E3321A3C743FA096FDE; SRCHD=AF=ANAB01; SRCHUID=V=2&GUID=1377E3EAF0854759B1D31477B4851C6C&dmnchg=1; MUIDV=NU=1; PPLState=1; ANON=A=C03F609D66AE24C23B4D0D53FFFFFFFF&E=1a0a&W=1; KievRPSSecAuth=FABaBBRaTOJILtFsMkpLVWSG6AN6C/svRwNmAAAEgAAACNZrOOqn7K3MGATnbpJekwteSA2ZivzyxsZzPiZWSLVEgRp1IagJO+A1GeV8RdRDkGD0hfYol8ur0IyBrUOc0gBpFjX/K5TtKXrGWzHsBmd3qyCMly0ytYt8MSFO22hZ4CAWtVHEVuHy4BO1egPkAmbl067hoOJrHxoioEL/YPQQ7Fre32tOnqDZvbw3R9lUrH4zZl1SzuK/ZOTm+iZ9kYd4C7OefN+QbsWPDJtbMBlgMEIAclNEgmWugyMP96pp0ofKlIsFwEQ2GwpWVfqTLfYiZc/OT6k37lNqdyAuBLyUEeyzCCsuZMDbepNvVKlz+iBxarWPLU9c+abLP6RHwIzJBD8PiVbPHo5XluMIzoA4VrxKxREbmgNzLfj+rsST7ZqKyOyPE+7I6PtzdsFVu7xyVLiQ70HM8o4u1buQAHu27I9gj2ms2+8w0HFijHR5wbb7nF2t3v7e50GzaqGzBtXWKf9ftqvCLvW2+nnmjf58Y867LnVXjYFPTRmynU0JdWd0MruWFebIgzweMtPyu51X3sHUSsjFhKB6zNnzwOkPuGZsXxFeZ4RCBbRLTyj5mr+RAjrk9DnIZEPKHo7UDnQkVI6eDUEavrmKdTzCT2X+zzEKtJHUCVetNxPFuWL2DtDoz+KQYUlZnxELCgG2P1FAYjIANOD/T7VxHerJ48SwN3h7mNEQTKn4cyp0cNf1d8vigHvTZQxQagd4IzAgjKQDPI7KYjlMQlKD3Ka70P4kzjPyb8j5Lub1s2dtCkKOxVEGjPhedC0vdgsr6lahyWbNfUeJlknZ9lXsdd7lCoGiAJprGSqRhXSS111ihxUB/FY3fbsazYGrq0oxJJjYIGCSNuhEMie7Jf+TRZ62zM9zM0jwUfLECvbMQbViM2CIkzDJkF4cO7mgJZQSzS3c0AN3VhVM/FnPiz7uXS93Ivih3t9BqSV4qvrY4efvOc5mLI8dg8+/6vfJKzevZxcAfxKuo5d5uLlw6updw01BT9YhXfKX/WnHz6SIA7kPYIAI3DP5IotHdCyhqoR26l4Szse9RyACY/H5I5Jmaqf4Vxtk3HFNvBsHGfrkI9uPp1NfhTiO9dJYLUxMbu3055tticwmJphpIWDrFHbE3ILattjxvyMYCy9DDJEoPHuUiI7eFPQ8VTUCzT66/wwWBb5GpR2qcs+Cay+3rqBhPtXMjvsZrSAx5bMsAsDfQY2P9YBQd73I5ffEKr6iJIsXT19hD+ol+tMbopbVCrze1Hjs/1ZSNjsy4s1YogKRClXQELwEBR9yrc23p93uPdDiZXJsWrTme2Uy7pnsubK5doLfrbQgzVNmxEt7gJcLnY0PTksUh4LE24Vbz3RoAiLwJTuredm0wxcV/8R8e0MZGZQ2zG1qDETPydK9JKt2V0bk1UR2a5iLFADgsk/J6CIyZlNQ3YeEAg4KFqNIng==; _RwBf=ilt=3&ihpd=0&ispd=2&rc=0&rb=0&gb=0&rg=0&pc=0&mtu=0&rbb=0&g=0&cid=&v=3&l=2022-01-07T08:00:00.0000000Z&lft=00010101&aof=0&o=2&p=&c=&t=0&s=0001-01-01T00:00:00.0000000+00:00&ts=2022-01-08T00:46:46.4055336+00:00&rwred=0; imgv=lodlg=1&gts=20210810&flts=20220716; _ITAB=STAB=TR; _UR=QS=0&TQS=0; _tarLang=default=zh-Hans; _TTSS_IN=hist=WyJlcyIsInpoLUhhbnMiLCJlbiIsImF1dG8tZGV0ZWN0Il0=; _TTSS_OUT=hist=WyJlbiIsInpoLUhhbnMiXQ==; _EDGE_S=SID=0786DCCD9F8262CE24D1CD329E50632B; WLS=C=6b2b74ecb580fd2a&N=Alan; _SS=SID=0786DCCD9F8262CE24D1CD329E50632B; ZHCHATSTRONGATTRACT=TRUE; ZHCHATWEAKATTRACT=TRUE; SUID=A; _FP=hta=on; SRCHUSR=DOB=20210810&T=1660446588000&POEX=W; ipv6=hit=1660450191314&t=4; ENSEARCH=BENVER=0; USRLOC=HS=1&BLOCK=TS=220814031116; _U=1b4ahsYpiHRYpqgCw0BGlHnFnJnqq2GeFlWT0XAPfl4VlgaUVoPic-Zd4cqWTgcYxqT-YBHB5vk6bojkQpHLdbQYTqLjCyhlUWZ2n9uGdKuJz6_QARlBnrsd4WAGWZyp6zr7PwnQexSBm4qsaIYfRMfOJ_Eltnx-7WcVEZjG2YDtZ2o4NVQsC_R297KmMdugV2TcxL7VZ3tNJW69RIflhdg; SNRHOP=I=&TS=; _HPVN=CS=eyJQbiI6eyJDbiI6NiwiU3QiOjAsIlFzIjowLCJQcm9kIjoiUCJ9LCJTYyI6eyJDbiI6NiwiU3QiOjAsIlFzIjowLCJQcm9kIjoiSCJ9LCJReiI6eyJDbiI6NiwiU3QiOjAsIlFzIjowLCJQcm9kIjoiVCJ9LCJBcCI6dHJ1ZSwiTXV0ZSI6dHJ1ZSwiTGFkIjoiMjAyMi0wOC0xNFQwMDowMDowMFoiLCJJb3RkIjowLCJHd2IiOjAsIkRmdCI6bnVsbCwiTXZzIjowLCJGbHQiOjAsIkltcCI6MjR9; SRCHHPGUSR=SRCHLANG=zh-Hans&BZA=0&BRW=HTP&BRH=M&CW=974&CH=807&SW=1920&SH=1080&DPR=1.25&UTC=480&DM=0&EXLTT=8&HV=1660448770&PV=10.0.0
-sec-ch-ua: "Chromium";v="104", " Not A;Brand";v="99", "Microsoft Edge";v="104"
-sec-ch-ua-arch: "x86"
-sec-ch-ua-bitness: "64"
-sec-ch-ua-full-version: "104.0.1293.54"
-sec-ch-ua-full-version-list: "Chromium";v="104.0.5112.81", " Not A;Brand";v="99.0.0.0", "Microsoft Edge";v="104.0.1293.54"
-sec-ch-ua-mobile: ?0
-sec-ch-ua-model: ""
-sec-ch-ua-platform: "Windows"
-sec-ch-ua-platform-version: "10.0.0"
-sec-fetch-dest: document
-sec-fetch-mode: navigate
-sec-fetch-site: none
-sec-fetch-user: ?1
 upgrade-insecure-requests: 1
 user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54
 x-edge-shopping-flag: 1
@@ -179,7 +152,7 @@ $engineNames = [
     'bing' => '必应同学'
 ];
 if ($engine = array_search(mb_substr($text, 0, 4), $engineNames)) {
-    $text = ltrim(str_replace($engineNames[$engine], '', $text), ',，.。 ');
+    $text = ltrim(str_replace($engineNames[$engine], '', $text), '?？,，.。 ');
 } else {
     $engine = 'bing';
 }
