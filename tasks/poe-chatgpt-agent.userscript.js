40b10d8561b12d4502e090e7ec9f551f8fb58b7a
cqiu <gdaymate.liu@gmail.com>
2023-07-27 23:56:35 +0800

    poe agent

--- tasks/poe-chatgpt-agent.userscript.js
+++ tasks/poe-chatgpt-agent.userscript.js
@@ -7,6 +7,7 @@
 // @match        https://poe.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=poe.com
 // @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // ==/UserScript==
 
@@ -20,11 +21,31 @@
 
     const uWin = unsafeWindow;
 
+    const myService = 'http://localhost/cqiu/tools/tasks/chatgpt-agent-service.php';
+
+    let saveResponse = function (resp) {
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: myService + "?act=save_response",
+            data: resp,
+            onload: function (response) {
+                console.log('保存的结果：', response.responseText);
+            },
+            onerror: function (error) {
+                function strip(html) {
+                    let doc = new DOMParser().parseFromString(html, 'text/html');
+                    return doc.body.textContent || "";
+                }
+                console.log('保存的结果err：', strip(error.responseText));
+            }
+        });
+    }
+
     // 创建 WebSocket 原型对象的引用
     var WS = uWin.WebSocket;
 
     // 保存答案：重写 WebSocket 构造函数
-    uWin.WebSocket = function() {
+    uWin.WebSocket = function () {
         let url = arguments[0];
         let protocols;
         let options;
@@ -41,7 +62,12 @@
         var ws = new WS(url, protocols, options);
         // 加上message监听
         ws.addEventListener("message", (event) => {
-            console.log("===> ", event.data);
+            // console.log("===> ", event.data);
+            try {
+                if ("complete" == JSON.parse(JSON.parse(event.data).messages[0]).payload.data.messageAdded.state) {
+                    saveResponse(event.data);
+                }
+            } catch { }
         });
 
         // 返回 WebSocket 实例
@@ -56,11 +82,14 @@
         if (/api\/gql_POST/.test(url)) {
             // print request data
             console.log('[fetch]', url, options);
+            if (options.body.variables && typeof options.body.variables.query === 'string' && options.body.variables.query.length > 0) {
+                saveResponse(options.body.variables.query);
+            }
         }
-        return oldFetch.apply(this, arguments).then(function (response) {
-            console.log('[fetch]', url, response.headers, response.clone().text());
+        return oldFetch.apply(this, arguments);/*.then(function (response) {
+            console.log('[fetch]', url, response.headers, response.clone().text().then(data => data));
             return response;
-        });
+        });*/
     };
 
     // @todo 输入问题:如何操作react的界面元素？触发方法。
