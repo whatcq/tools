fe9a2a60e87d61759b085f1d5578ff360c652793
cqiu <gdaymate.liu@gmail.com>
2023-11-13 23:03:45 +0800

    save lrc+mp3

--- yuyin.php
+++ yuyin.php
@@ -12,4 +12,19 @@ if ($_GET['act'] ?? '' === 'list') {
 }
 
 date_default_timezone_set('Asia/Chongqing');
-echo file_put_contents($folder . date('YmdHi') . '.txt', file_get_contents('php://input'));
+// print_r($_POST);
+// print_r($_FILES);
+
+$date = date('YmdHi');
+if ($_FILES) {
+    // foreach($_FILES as $k=>$v){
+    //     print_r($v);
+    // }
+    file_put_contents($folder . $date . '.lrc', $_POST['lrc']);
+    if (move_uploaded_file($_FILES['upfile']['tmp_name'], $folder . $date . $_FILES['upfile']['name'])) {
+        echo 'success';
+    }
+    die;
+}
+
+echo file_put_contents($folder . $date . '.txt', file_get_contents('php://input'));
