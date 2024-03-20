811972d065e0516d06c0f38e9662a33f9d064559
cqiu <gdaymate.liu@gmail.com>
2024-03-20 11:59:23 +0800

    run-php-css

--- playground/include.php
+++ playground/include.php
@@ -1,5 +1,5 @@
 <?php
-
+if (empty($fff)) echo '<body style="background-color:#ffffffe3;color:#000">';
 if (isset($noPre)) return true;
 ?>
-<pre style="color:#03b503;font: 14px/16px Consolas;white-space: pre-wrap;word-wrap: break-word;">
+<pre style="color:<?=empty($fff)?'#000':'#03b503'?>;font: 14px/16px Consolas;white-space: pre-wrap;word-wrap: break-word;">
