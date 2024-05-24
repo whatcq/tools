77e74eef09976d5419af0a621979b68133928292
cqiu <gdaymate.liu@gmail.com>
2024-05-24 11:31:35 +0800

    luckysheet-excel.html

--- playground/saver.php
+++ playground/saver.php
@@ -8,6 +8,24 @@ if ($ip !== '127.0.0.1' && $ip !== '::1') {
 $act = isset($_REQUEST['act']) ? $_REQUEST['act'] : '';
 $path = __DIR__ . '/';
 
+if ($act === 'files') {
+    $files = [];
+    $ext = $_REQUEST['ext'] ?? 'php';
+    foreach (glob("{$path}*.{$ext}") as $filename) {
+        $files[] = basename($filename, '.' . $ext);
+    }
+    header('Content-Type: application/json');
+    echo json_encode($files, 448);
+    die;
+}
+if ($act === 'upload') {
+    $uploaded = [];
+    foreach ($_FILES as $file) {
+        $uploaded[$file['name']] = move_uploaded_file($file['tmp_name'], $path . $file['name']);
+    }
+    die(json_encode($uploaded));
+}
+
 if ($act === 'save') {
     $file = $path . $_REQUEST['filename'];
     $c = file_put_contents($file, $_REQUEST['source']);
