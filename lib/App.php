c5e1ed1f58d9197c33142ff0e2d4e46feb6b1e5a
cqiu <gdaymate.liu@gmail.com>
2025-05-06 23:22:51 +0800

    App errorHandler

--- lib/App.php
+++ lib/App.php
@@ -9,13 +9,13 @@ defined('APP_DEBUG') or define('APP_DEBUG', true);
 if (APP_DEBUG) {
     error_reporting(-1);
     ini_set("display_errors", "On");
-    set_error_handler("_err_handle");
 } else {
     error_reporting(E_ALL & ~(E_STRICT | E_NOTICE));
     ini_set("display_errors", "Off");
     ini_set("log_errors", "On");
-    set_error_handler("_err_handle2");
+    //set_error_handler("_err_handle2");
 }
+set_error_handler(['App', 'errorHandler']);
 
 /**
  * 配置文件: .env/.ini; config.php; db-config;
@@ -62,9 +62,18 @@ class App
 
     public static function run()
     {
-        list($action, $controller, static::$module) = isset($_REQUEST['r'])
-            ? array_reverse(explode('/', trim($_REQUEST['r'], '/'), 3))
-            : ['index', 'index', null];
+        // route: r=module.controller/action
+        $controller = 'index';
+        $action = 'index';
+        if (isset($_REQUEST['r'])) {
+            $parts = explode('/', trim($_REQUEST['r'], '/'), 2);
+            isset($parts[1]) && $action = $parts[1];
+            $parts0 = explode('.', $parts[0], 2);
+            isset($parts0[1])
+                ? ($controller = $parts0[1]) && (static::$module = $parts0[0])
+                : $controller = $parts0[0];
+        }
+        _log(static::$module, $controller, $action);
 
         spl_autoload_register(['App', 'inner_autoload']);
 
@@ -91,4 +100,41 @@ class App
             }
         }
     }
+
+    public static function log($module, $controller, $action)
+    {
+        $log_file = APP_DIR . DS . 'protected' . DS . 'runtime' . DS . 'log' . DS . date('Ymd') . '.log';
+        $log_str = date('Y-m-d H:i:s') . ' ' . $module . '.' . $controller . '.' . $action . "\n";
+        file_put_contents($log_file, $log_str, FILE_APPEND);
+    }
+
+    public static function errorHandler($errno, $errStr, $errFile, $errLine)
+    {
+        if (!(error_reporting() & $errno)) {
+            // This error code is not included in error_reporting
+            return;
+        }
+        switch ($errno) {
+            case E_USER_ERROR:
+                echo "<b>My ERROR</b> [$errno] $errStr<br />\n";
+                echo "  Fatal error on line $errLine in file $errFile";
+                echo ", PHP " . PHP_VERSION . " (" . PHP_OS . ")<br />\n";
+                echo "Aborting...<br />\n";
+                exit(1);
+                break;
+
+            case E_USER_WARNING:
+                echo "<b>My WARNING</b> [$errno] $errStr<br />\n";
+                break;
+
+            case E_USER_NOTICE:
+                echo "<b>My NOTICE</b> [$errno] $errStr<br />\n";
+                break;
+
+            default:
+                echo "Unknown errortype: [$errno] $errStr<br />\n";
+        }
+    }
 }
+
+App::run();
