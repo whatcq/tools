b28e1958f863bdfb8b11853b18953a8de06c35b7
cqiu <gdaymate@126.com>
2025-05-27 18:04:07 +0800

    userscript tbc

---
+++ tasks/template.userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         userscript-foo
+// @namespace    https://github.com/JSREI/userscript-template.git
+// @version      0.0.1
+// @description
+// @document
+// @author       CC11001100 <CC11001100@qq.com>
+// @match        *://*/*
+// @run-at       document-start
+// @require    file://D:/workspace/userscript-template/dist/index.js
+// ==/UserScript==
+(() => {})();
